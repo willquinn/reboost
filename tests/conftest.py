302350751da66d87e6d643082b9c18fc8a4cb978
Manuel Huber <info@manuelhu.de>
2024-10-01 00:16:38 +0200

    tests: enable numba bounds checking

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import numba
+
+
+def patch_numba_for_tests():
+    """Globally disable numba cache and enable bounds checking (for testing)."""
+    njit_old = numba.njit
+
+    def njit_patched(*args, **kwargs):
+        kwargs.update({"cache": False, "boundscheck": True})
+        return njit_old(*args, **kwargs)
+
+    numba.njit = njit_patched
+
+
+patch_numba_for_tests()
